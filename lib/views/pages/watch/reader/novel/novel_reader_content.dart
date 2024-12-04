5abe667467234ebf86bd09bd3d00145e8fc1fb72
Hualiang <78242797+hualiong@users.noreply.github.com>
2024-12-04 11:38:12 +0800

    Update novel_reader_content.dart

--- lib/views/pages/watch/reader/novel/novel_reader_content.dart
+++ lib/views/pages/watch/reader/novel/novel_reader_content.dart
@@ -76,15 +76,16 @@ class _NovelReaderContentState extends State<NovelReaderContent> {
                   );
                 }
                 if (index == 1) {
-                  return (watchData.subtitle != null)
-                      ? Padding(
-                          padding: const EdgeInsets.only(bottom: 20),
-                          child: Text(
-                            watchData.subtitle!,
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        )
-                      : const SizedBox();
+                  return const SizedBox();
+                  // return (watchData.subtitle != null)
+                  //     ? Padding(
+                  //         padding: const EdgeInsets.only(bottom: 20),
+                  //         child: Text(
+                  //           watchData.subtitle!,
+                  //           style: const TextStyle(fontSize: 20),
+                  //         ),
+                  //       )
+                  //     : const SizedBox();
                 }
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 20),
